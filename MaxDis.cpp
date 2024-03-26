3c490ae20bd7e7ddceb94c6b7cc8b801c45343d7
Sok Sokong <100309426+Jeff-kxng@users.noreply.github.com>
2024-03-26 23:59:55 +0700

    Update MaxDis.cpp

--- MaxDis.cpp
+++ MaxDis.cpp
@@ -1,3 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isFeasible(int mid, int arr[], int n, int c) {
+    int pos = arr[0];
+    int elements = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] - pos >= mid) {
+            pos = arr[i];
+            elements++;
+            if (elements == c)
+                return true;
+        }
+    }
+    return false;
+}
+
+int largestMinDist(int arr[], int n, int c) {
+    int left = 0;
+    int right = arr[n-1] - arr[0];
+    int max_dist = -1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (isFeasible(mid, arr, n, c)) {
+            max_dist = mid;
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return max_dist;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, c;
+        cin >> n >> c;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        sort(arr, arr+n);
+        cout << largestMinDist(arr, n, c) << endl;
+    }
+    return 0;
+}
 /*
 Problem: LAB.03.03 - Max-Distance Sub-Sequence
 Description
